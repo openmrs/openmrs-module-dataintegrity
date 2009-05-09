0b70e674a13c2e40100c572d60714d4c5f430e37
Nimantha Baranasuriya <me@nimantha.info>
2009-05-09 05:24:42 +0000

    dataintegrity: adding the initial module files

--- src/org/openmrs/module/dataintegrity/DataIntegrityService.java
+++ src/org/openmrs/module/dataintegrity/DataIntegrityService.java
@@ -1,20 +1,5 @@
 package org.openmrs.module.dataintegrity;
 
-import java.util.List;
-
-import org.openmrs.api.APIException;
-import org.openmrs.module.dataintegrity.db.DataIntegrityDAO;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
 public interface DataIntegrityService {
-	
-	public void setDataIntegrityDAO(DataIntegrityDAO dao);
-	
-	public void saveDataIntegrityTemplate(DataIntegrityTemplate dataIntegrityTemplate) throws APIException;
-
-    public DataIntegrityTemplate getDataIntegrityTemplate(Integer templateId) throws APIException;
-
-    public List<DataIntegrityTemplate> getAllDataIntegrityTemplates() throws APIException;
 
 }
--- src/org/openmrs/module/dataintegrity/db/DataIntegrityDAO.java
+++ src/org/openmrs/module/dataintegrity/db/DataIntegrityDAO.java
@@ -1,15 +1,5 @@
 package org.openmrs.module.dataintegrity.db;
 
-import org.hibernate.SessionFactory;
-import org.openmrs.api.db.DAOException;
-import org.openmrs.module.dataintegrity.DataIntegrityTemplate;
-
-
 public interface DataIntegrityDAO {
-	public void setSessionFactory(SessionFactory sessionFactory);
-
-    public void saveDataIntegrityTemplate(DataIntegrityTemplate dataIntegrityTemplate) throws DAOException;
-
-    public DataIntegrityTemplate getDataIntegrityTemplate(Integer templateId) throws DAOException;
 
 }
