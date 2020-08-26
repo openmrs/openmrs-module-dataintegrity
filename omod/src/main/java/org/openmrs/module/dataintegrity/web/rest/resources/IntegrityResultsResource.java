e9a2f3e59837ba8c86a6038aef4fdb47beb2dbdc
dkayiwa <kayiwadaniel@gmail.com>
2020-08-26 15:18:31 +0300

    Adding support for platforms 2.2 - 2.4

--- omod/src/main/java/org/openmrs/module/dataintegrity/web/rest/resources/IntegrityResultsResource.java
+++ omod/src/main/java/org/openmrs/module/dataintegrity/web/rest/resources/IntegrityResultsResource.java
@@ -19,7 +19,7 @@ import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOp
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 @Resource(name = RestConstants.VERSION_1  + DataIntegrityRestController.DATA_INTEGRITY_REST_NAMESPACE + "/integrityresults", supportedClass = DataIntegrityResult.class, supportedOpenmrsVersions = { "1.8.*",
-        "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
+        "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*", "2.2.*", "2.3.*", "2.4.*" })
 public class IntegrityResultsResource extends DelegatingCrudResource<DataIntegrityResult> {
 
     @Override
